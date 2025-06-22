8a796a869a298c89877a2c0b32420e3aa2f2ca4f
Lucas Marques da Silva <111651260+LucasMKS@users.noreply.github.com>
2025-06-22 12:17:45 +0000

    Remover importações não utilizadas no UsuarioRepository

--- src/main/java/com/a3/todo/repository/UsuarioRepository.java
+++ src/main/java/com/a3/todo/repository/UsuarioRepository.java
@@ -1,10 +1,7 @@
 package com.a3.todo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.a3.todo.entity.Usuario;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
